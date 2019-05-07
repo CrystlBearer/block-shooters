330f42f122efa21c9aa2c838f4cb9ba7b3251af2
Vivian Hoang <vivianhoang0@gmail.com>
2019-05-06 17:43:31 -0700

    Testing spawning and collision

--- Game/GameMain.c
+++ Game/GameMain.c
@@ -1,10 +1,12 @@
-#include "Start_Menu.h" //This will include the Dependencies.h file, Objects.h, and sl.h header files
+#include "GameMain.h" // This will include the Dependencies.h file, Objects.h, and sl.h header files
 
-const int WINDOW_WIDTH_GAME = 600; //This will be the fixed size of the window's width
-const int WINDOW_HEIGHT_GAME = 480; //This will be the fixed size of the window's height
+const int WINDOW_WIDTH_GAME = 600; // This will be the fixed size of the window's width
+const int WINDOW_HEIGHT_GAME = 480; // This will be the fixed size of the window's height
 
-const short OBJECT_WIDTH = 50; //This is the width of all objects in game
-const short OBJECT_HEIGHT = 30; //This is the height of all objects in the game
+const short OBJECT_WIDTH = 60; // This is the width of all objects in game
+const short OBJECT_HEIGHT = 30; // This is the height of all objects in the game
+SDL_Event event; // This is used to detect user's actions
+
 
 /*
 * This will bring up the start menu and call the game loop
@@ -17,12 +19,63 @@ int start_menu() {
 	}
 	else {
 		Game_State state; // The game state is initialized (will need to implement start menu later)
-		state = CONTINUE;
-		game_loop(&state); // This will run the function game loop to take in user input
+		state = PLAY;
+		do {
+			switch (state) {
+				case PLAY:
+					state = game_loop();
+					break;
+				case MENU:
+					state = show_menu();
+					break;
+				default:
+					state = STOP;
+					break;
+			}
+		} while (state != STOP);
 		close(); // Clean up 
 		return 0;
 	}
 }
+
+
+
+
+
+/*
+* This will display the main menu
+* @return select {Game_State} This will have game state enum to determine to continue the game or exit
+*/
+Game_State show_menu() {
+	puts("Entering Main Menu...");
+	Game_State select = MENU;
+	bool quit = false;
+	while (!quit) {
+		while (SDL_PollEvent(&event) != 0) {
+			if (event.type == SDL_QUIT) { //User requests to exit
+				puts("Exiting window...");
+				quit = true;
+				select = STOP;
+			}
+		}
+		render_bg(render, 0, 20, 30, 0);
+		SDL_RenderPresent(render); //This will render all the shapes into the canvas
+	}
+	return select;
+}
+
+
+
+void spawn(SDL_Rect * enemies, Object * enemy_object) {
+	(enemies)->x = 0;
+	(enemies)->y = 0;
+	(enemies)->w = OBJECT_WIDTH;
+	(enemies)->h = OBJECT_HEIGHT;
+	set_object(enemy_object, enemies);
+}
+
+
+
 
 
 /*
@@ -30,10 +83,17 @@ int start_menu() {
 * @param finished {int *} 
 * @return true {bool}
 */
-bool game_loop(Game_State * state) {
+Game_State game_loop() {
+	Game_State state = MENU;
 	SDL_Rect user = { 0,WINDOW_HEIGHT_GAME-OBJECT_HEIGHT,OBJECT_WIDTH,OBJECT_HEIGHT}; // This initializes the user's character 
 	Object * user_object = malloc(sizeof(Object)); // This is used to update the user's character position of all four corners (Found in Objects.h and Objects.c)
-	SDL_Event event;
+	SDL_Rect * enemies;
+	enemies = malloc(sizeof(SDL_Rect) * 1);
+	Object * enemy_object = malloc(sizeof(Object));
+	spawn(enemies,enemy_object);
+	
+
+
 	set_object(user_object, &user); // Initializing the values in Objects.c
 	bool quit = false; // This will quit the program if the user exits
 
@@ -41,28 +101,42 @@ bool game_loop(Game_State * state) {
 	while (!quit) { //While application is runnin
 		while (SDL_PollEvent(&event) != 0) { //This is our event loop
 			if (event.type == SDL_QUIT) { //User requests to exit
-				puts("Exiting window...");
+				puts("Exiting Game Window...");
+				state = STOP;
 				quit = true;
-			}
-			else if (event.type == SDL_KEYDOWN) {//https://wiki.libsdl.org/SDL_EventType?highlight=%28%5CbCategoryEnum%5Cb%29%7C%28CategoryEvents%29
+			} else if (event.type == SDL_KEYDOWN) { //https://wiki.libsdl.org/SDL_EventType?highlight=%28%5CbCategoryEnum%5Cb%29%7C%28CategoryEvents%29
 				switch (event.key.keysym.sym) {
-				case SDLK_a:
-					move_shape(&user, user_object, -25, 0);
-					break;
-				case SDLK_d:
-					move_shape(&user, user_object, 25, 0);
-					break;
-				default:
-					break;
+					case SDLK_a:
+						move_shape(&user, user_object, -(OBJECT_WIDTH), 0);
+						break;
+					case SDLK_d:
+						move_shape(&user, user_object, (OBJECT_WIDTH), 0);
+						break;
+					case SDLK_f:
+						move_shape(enemies, enemy_object, 0,(OBJECT_HEIGHT));
+						break;
+					case SDLK_q:
+						puts("Returning to main menu...");
+						state = MENU;
+						quit = true;
+					default:
+						break;
 				}
 			}
 		}
 		render_bg(render, 0x00, 0x00, 0x00, 0x00); //this will clear the window of all objects
-		render_shape(&user,render, 192, 16, 16, 255); //this will draw the shape, but will not render
+		render_shape(enemies, render, 192, 16, 16, 255);
+		render_shape(&user,render, 0, 0, 255, 0); //this will draw the shape, but will not render
+		if (SDL_HasIntersection(enemies, &user)) {
+			puts("THEY INTERSECT!");
+		}
 		SDL_RenderPresent(render); //This will render all the shapes into the canvas
 	}
-	return true;
+	return state;
 }
+
+
+
 
 
 /* 
@@ -94,6 +168,9 @@ bool init() {
 
 
 
+
+
+
 /*
  * This will render the background with black, or what ever value is assigned to
  * the rgba.
@@ -111,6 +188,8 @@ void render_bg(
 	SDL_SetRenderDrawColor(render, r, g, b, a);
 	SDL_RenderClear(render);
 }
+
+
 
 
 
